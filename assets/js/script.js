e7efab229ac2c801d1d2c460ece0f41e01a60fa6
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-08-29 16:39:26 +0000

    refactor question and answer arrays to one array and refactor loadqestion function to work with new array

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,58 +33,40 @@ function runGame(gameDif) {
 }
 
 
+
 /**
  * generates a random easy question and answer
  */
 function easyMode() {
 
-let questionsEasy = [
-    "What name is given to someone bonded to an Aes Sedai?",
-    "What item does Matt take from Shadar Logoth?",
-    "Rand learns to play which instrument?"
-];
-
-let answersEasy = [
-    ["Warder", "Asha'man", "Guardian", "Grey man"],
-    ["Gold coin", "Silver dice set", "Ruby-hilted dagger", "Crystal sward "],
-    ["Harp", "Flute", "fiddle", "Triangle"]
+let qAEasySet = 
+[
+  {
+    question: "What name is given to someone bonded to an Aes Sedai?",
+    answers:["Warder", "Asha'man", "Guardian", "Grey man"],
+  },
+  {
+    question:"What item does Matt take from Shadar Logoth?",
+    answers: ["Gold coin", "Silver dice set", "Ruby-hilted dagger", "Crystal sward "],
+  },
+  {
+    question: "Rand learns to play which instrument?",
+    answers: ["Harp", "Flute", "fiddle", "Triangle"],
+  }
 ];
 
 /**
- * gets a random question from questionEasy
+ * gets a random question from qAEasySet
  */
 function loadQuestion() {
-    let questionGen = Math.floor(Math.random() * questionsEasy.length);
-    document.getElementById("question").innerHTML = questionsEasy[questionGen];
-}
-
-
-/**
- * get the maching answers for the question
- */
-function loadAnswers() {
+    let questionGen = Math.floor(Math.random() * qAEasySet.length);
     let answers = document.getElementById("answer-container");
-    if (document.getElementById("question").innerText === questionsEasy[0]) {
-        answers.innerHTML = `<button id="btn0" class="answer-btn">${answersEasy[0][0]}</button>
-        <button id="btn1" class="answer-btn">${answersEasy[0][1]}</button>
-        <button id="btn2" class="answer-btn">${answersEasy[0][2]}</button>
-        <button id="btn3" class="answer-btn">${answersEasy[0][3]}</button>`
-    }   
-    else if (document.getElementById("question").innerText === questionsEasy[1]) { 
-        answers.innerHTML = `<button id="btn0" class="answer-btn">${answersEasy[1][0]}</button>
-        <button id="btn1" class="answer-btn">${answersEasy[1][1]}</button>
-        <button id="btn2" class="answer-btn">${answersEasy[1][2]}</button>
-        <button id="btn3" class="answer-btn">${answersEasy[1][3]}</button>`
-    }
-    else if (document.getElementById("question").innerText === questionsEasy[2]) {
-        answers.innerHTML = `<button id="btn0" class="answer-btn">${answersEasy[2][0]}</button>
-        <button id="btn1" class="answer-btn">${answersEasy[2][1]}</button>
-        <button id="btn2" class="answer-btn">${answersEasy[2][2]}</button>
-        <button id="btn3" class="answer-btn">${answersEasy[2][3]}</button>`
-    }
+    document.getElementById("question").innerHTML = qAEasySet[questionGen]["question"];
+     answers.innerHTML = 
+        `<button id="btn0" class="answer-btn">${qAEasySet[questionGen]["answers"][0]}</button>
+        <button id="btn1" class="answer-btn">${qAEasySet[questionGen]["answers"][1]}</button>
+        <button id="btn2" class="answer-btn">${qAEasySet[questionGen]["answers"][2]}</button>
+        <button id="btn3" class="answer-btn">${qAEasySet[questionGen]["answers"][3]}</button>`
 };
-
 loadQuestion();
-
-loadAnswers();
 };
