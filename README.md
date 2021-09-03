2fc22e0945692cdd18b891b022dddaf69fb7e2f9
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-09-03 00:04:24 +0000

    add checkAnswer function to js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### source
 * This help me understand how to randomise my questions: https://stackoverflow.com/questions/43847375/creating-random-questions-and-answers-javascript
+
+* used these answers as inspration for clearselected function- https://stackoverflow.com/questions/40153194/how-to-remove-class-from-siblings-of-an-element-without-jquery
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,32 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-    });
+    }
+);
 
-    /**
-     * this adds the selected class to a anserbutton when clicked by the user.
-     * it also calls the clearselected function to remove the selected class from other buttons.
-     */
+let qAEasySet = 
+[
+    {
+    question: "What name is given to someone bonded to an Aes Sedai?",
+    answers:["Warder", "Asha'man", "Guardian", "Grey man"],
+    corectAnswer: "Warder"
+    },
+    {
+    question:"What item does Matt take from Shadar Logoth?",
+    answers: ["Gold coin", "Silver dice set", "Ruby-hilted dagger", "Crystal sward "],
+    corectAnswer: "Ruby-hilted dagger"
+    },
+    {
+    question: "Rand learns to play which instrument?",
+    answers: ["Harp", "Flute", "fiddle", "Triangle"],
+    corectAnswer: "Flute"
+    }
+];
+
+/**
+ * this adds the selected class to a anserbutton when clicked by the user.
+ * it also calls the clearselected function to remove the selected class from other buttons.
+ */
 function userAnswer() {
     let answers = document.getElementsByClassName("answer-btn")
     for (let answer of answers) {
@@ -44,6 +64,18 @@ function clearSelected() {
     };
 };
 
+function checkAnswer(i) {
+    let submitBtn = document.getElementById("submit-btn")
+    let selctedAnswer = document.getElementsByClassName("selected").innerHTML;
+    submitBtn.addEventListener("click", () => {
+        if (selctedAnswer === i) {
+            alert(`Weldone!`);
+        } else if (selctedAnswer !== i) {
+            alert(`Nope`);
+        }
+    })
+}
+
 /**
  * runs the difculty level the user selects.
  */
@@ -65,30 +97,12 @@ function runGame(gameDif) {
  */
 function easyMode() {
 
-let qAEasySet = 
-[
-  {
-    question: "What name is given to someone bonded to an Aes Sedai?",
-    answers:["Warder", "Asha'man", "Guardian", "Grey man"],
-    corectAnswer: 0
-  },
-  {
-    question:"What item does Matt take from Shadar Logoth?",
-    answers: ["Gold coin", "Silver dice set", "Ruby-hilted dagger", "Crystal sward "],
-    corectAnswer: 2 
-    },
-  {
-    question: "Rand learns to play which instrument?",
-    answers: ["Harp", "Flute", "fiddle", "Triangle"],
-    corectAnswer: 0
-  }
-];
-
 /**
  * gets a random question from qAEasySet
  */
 function loadQuestion() {
     let questionGen = Math.floor(Math.random() * qAEasySet.length);
+    let corectAns = qAEasySet[questionGen]["corectAnswer"];
     let answers = document.getElementById("answer-container");
     document.getElementById("question").innerHTML = qAEasySet[questionGen]["question"];
      answers.innerHTML = 
@@ -96,9 +110,9 @@ function loadQuestion() {
         <button id="btn1" class="answer-btn">${qAEasySet[questionGen]["answers"][1]}</button>
         <button id="btn2" class="answer-btn">${qAEasySet[questionGen]["answers"][2]}</button>
         <button id="btn3" class="answer-btn">${qAEasySet[questionGen]["answers"][3]}</button>`
-    qAEasySet.splice(questionGen,1);
     userAnswer();
-    console.log(qAEasySet)
+    checkAnswer(corectAns);
+    qAEasySet.splice(questionGen,1);
 };
 loadQuestion();
 };
