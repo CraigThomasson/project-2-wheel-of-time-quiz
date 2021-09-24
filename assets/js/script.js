de807ce27e6a97dfbe09713acda2131165c787a0
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-09-24 11:16:26 +0000

    move question list in to on js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,78 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 );
 
-let easyQuestions = [
-    {
-    question: "What name is given to someone bonded to an Aes Sedai?",
-    answers:["Warder", "Asha'man", "Guardian", "Grey man"],
-    corectAnswer: "Warder",
-    img: "assets/media/images/swordsman.jpg"
-    },
-    {
-    question:"What item does Matt take from Shadar Logoth?",
-    answers: ["Gold coin", "Silver dice set", "Ruby-hilted dagger", "Crystal sward "],
-    corectAnswer: "Ruby-hilted dagger",
-    img: "assets/media/images/swordsman.jpg"
-    },
-    {
-    question: "Rand learns to play which instrument?",
-    answers: ["Harp", "Flute", "Fiddle", "Triangle"],
-    corectAnswer: "Flute",
-    img: "assets/media/images/swordsman.jpg"
-    },
-    {
-    question: "What does Perin bring with him from master Luhhan’s smithy when leaving Emond’s Field?",
-    answers: ["War pick", "Heron marked blade", "Blacksmiths hammer", "Half moon axe"],
-    corectAnswer: "Half moon axe",
-    img: "assets/media/images/swordsman.jpg"
-    },
-    {
-    question: "Which of these colours does not belong to and Ajar of the White Tower?",
-    answers: ["green", "Purple", "Blue", "Red"],
-    corectAnswer: "Purple",
-    img: "assets/media/images/swordsman.jpg"
-    },
-    {
-    question: "Hopper is a…? ",
-    answers: ["Tiger", "Rabbit", "Frog", "Wolf"],
-    corectAnswer: "Wolf",
-    img: "assets/media/images/swordsman.jpg"
-    }
-];
-
-let mediumQuestions = [
-    {
-    question: "What is the name of the army that follows Matrim cauthon?",
-    answers:["The band of the red hammer", "The band of the red heron", "The band of the red hand", "The band of the red wheel"],
-    corectAnswer: "The band of the red hand"
-    },
-    {
-    question:"How many sisters does Matt have?",
-    answers: ["One", "Two", "Three", "four"],
-    corectAnswer: "Two"
-    },
-    {
-    question: "meduim question",
-    answers: ["Harp", "Flute", "Fiddle", "Triangle"],
-    corectAnswer: "Flute"
-    },
-    {
-    question: "meduim question",
-    answers: ["War pick", "Heron marked blade", "Blacksmiths hammer", "Half moon axe"],
-    corectAnswer: "Half moon axe"
-    },
-    {
-    question: "medium question",
-    answers: ["green", "Purple", "Blue", "Red"],
-    corectAnswer: "Purple"
-    },
-    {
-    question: "medium question ",
-    answers: ["Tiger", "Rabbit", "Frog", "Wolf"],
-    corectAnswer: "Wolf"
-    }
-];
-
+// will shuffel the questions before being called latter in the code 
 shuffle(easyQuestions)
 shuffle(mediumQuestions)
 
