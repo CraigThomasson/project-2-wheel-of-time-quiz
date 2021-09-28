920d2e66746963db0197b8bfd3a263678a83a045
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-09-28 22:45:34 +0000

    fix image sorce for easy question

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,37 +9,37 @@ let easyQuestions = [
     question:"What item does Matt take from Shadar Logoth?",
     answers: ["Gold coin", "Silver dice set", "Ruby-hilted dagger", "Crystal sward "],
     corectAnswer: "Ruby-hilted dagger",
-    img: "assets/media/images/swordsman.jpg"
+    img: "assets/media/images/dark-ruins.jpg"
     },
     {
     question: "Rand learns to play which instrument?",
     answers: ["Harp", "Flute", "Fiddle", "Triangle"],
     corectAnswer: "Flute",
-    img: "assets/media/images/swordsman.jpg"
+    img: "assets/media/images/notes.jpg"
     },
     {
     question: "What does Perin bring with him from master Luhhan’s smithy when leaving Emond’s Field?",
     answers: ["War pick", "Heron marked blade", "Blacksmiths hammer", "Half moon axe"],
     corectAnswer: "Half moon axe",
-    img: "assets/media/images/swordsman.jpg"
+    img: "assets/media/images/smithy.jpg"
     },
     {
     question: "Which of these colours does not belong to and Ajar of the White Tower?",
     answers: ["green", "Purple", "Blue", "Red"],
     corectAnswer: "Purple",
-    img: "assets/media/images/swordsman.jpg"
+    img: "assets/media/images/white-tower.jpg"
     },
     {
     question: "Hopper is a…?",
     answers: ["Tiger", "Rabbit", "Frog", "Wolf"],
     corectAnswer: "Wolf",
-    img: "assets/media/images/swordsman.jpg"
+    img: "assets/media/images/forest.jpg"
     },
     {
     question: "What condition effects Ogier’s when they are away from steadings?",
     answers: ["The Needing", "The Wanting", "The Yearning", "The Longing"],
     corectAnswer: "The Longing",
-    img: "assets/media/images/swordsman.jpg"
+    img: "assets/media/images/ogire.jpg"
     }
 ];
 
@@ -47,32 +47,38 @@ let mediumQuestions = [
     {
     question: "What is the name of the army that follows Matrim cauthon?",
     answers:["The band of the red hammer", "The band of the red heron", "The band of the red hand", "The band of the red wheel"],
-    corectAnswer: "The band of the red hand"
+    corectAnswer: "The band of the red hand",
+    img: "assets/media/images/horse-soldier.jpg"
     },
     {
     question:"How many sisters does Matt have?",
     answers: ["One", "Two", "Three", "four"],
-    corectAnswer: "Two"
+    corectAnswer: "Two",
+    img: "assets/media/images/matt.jpg"
     },
     {
     question: "Elyas Machera was also known by what name?",
     answers: ["Sharp Tooth", "Steel Tooth", "One Tooth", "Long Tooth"],
-    corectAnswer: "Long Tooth"
+    corectAnswer: "Long Tooth",
+    img: "assets/media/images/wolves.jpg"
     },
     {
     question: "Siuan Sanche was born in which city?",
     answers: ["Tar Valon", "Caemlyn", "Tear", "Far Madding"],
-    corectAnswer: "Tear"
+    corectAnswer: "Tear",
+    img: "assets/media/images/fishing.jpg"
     },
     {
     question: "In which inn did min work in? ",
     answers: ["The Stag and Lion", "The Oak and Thorn", "The Blue Rose", "The Crown and Lion"],
-    corectAnswer: "The Stag and Lion"
+    corectAnswer: "The Stag and Lion",
+    img: "assets/media/images/inn.jpg"
     },
     {
     question: "When captured as a damane, Egwene was given what name?",
     answers: ["Alivia", "Tuli", "Seri", "Dali"],
-    corectAnswer: "Tuli"
+    corectAnswer: "Tuli",
+    img: "assets/media/images/damane.jpg"
     }
 ];
 
