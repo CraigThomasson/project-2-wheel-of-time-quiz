36a3e72fc270d985b5290b669a1dafa37eeeb00c
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-10-19 21:16:32 +0000

    spellcheck

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,7 +6,7 @@ let easyQuestions = [
     img: "assets/media/images/swordsman.jpg"
     },
     {
-    question:"What item does Matt take from Shadar Logoth?",
+    question:"What item does Mat take from Shadar Logoth?",
     answers: ["Gold coin", "Silver dice set", "Ruby-hilted dagger", "Crystal sward "],
     corectAnswer: "Ruby-hilted dagger",
     img: "assets/media/images/dark-ruins.jpg"
@@ -18,13 +18,13 @@ let easyQuestions = [
     img: "assets/media/images/notes.jpg"
     },
     {
-    question: "What does Perin bring with him from master Luhhan’s smithy when leaving Emond’s Field?",
+    question: "What does Perrin bring with him from master Luhhan’s smithy when leaving Emond’s Field?",
     answers: ["War pick", "Heron marked blade", "Blacksmiths hammer", "Half moon axe"],
     corectAnswer: "Half moon axe",
     img: "assets/media/images/smithy.jpg"
     },
     {
-    question: "Which of these colours does not belong to and Ajar of the White Tower?",
+    question: "Which of these colours does not belong to and Ajah of the White Tower?",
     answers: ["green", "Purple", "Blue", "Red"],
     corectAnswer: "Purple",
     img: "assets/media/images/white-tower.jpg"
@@ -36,7 +36,7 @@ let easyQuestions = [
     img: "assets/media/images/forest.jpg"
     },
     {
-    question: "What condition effects Ogier’s when they are away from steadings?",
+    question: "What condition effects Ogier when they are away from steadings?",
     answers: ["The Needing", "The Wanting", "The Yearning", "The Longing"],
     corectAnswer: "The Longing",
     img: "assets/media/images/ogire.jpg"
@@ -45,13 +45,13 @@ let easyQuestions = [
 
 let mediumQuestions = [
     {
-    question: "What is the name of the army that follows Matrim cauthon?",
-    answers:["The band of the red hammer", "The band of the red heron", "The band of the red hand", "The band of the red wheel"],
-    corectAnswer: "The band of the red hand",
+    question: "What is the name of the army that follows Matrim Cauthon?",
+    answers:["The Band of the Red Hammer", "The Band of the Red Heron", "The Band of the Red Hand", "The Band of the Red Wheel"],
+    corectAnswer: "The Band of the Red Hand",
     img: "assets/media/images/horse-soldier.jpg"
     },
     {
-    question:"How many sisters does Matt have?",
+    question:"How many sisters does Mat have?",
     answers: ["One", "Two", "Three", "four"],
     corectAnswer: "Two",
     img: "assets/media/images/matt.jpg"
@@ -69,7 +69,7 @@ let mediumQuestions = [
     img: "assets/media/images/fishing.jpg"
     },
     {
-    question: "In which inn did min work in? ",
+    question: "In which inn did Min work? ",
     answers: ["The Stag and Lion", "The Oak and Thorn", "The Blue Rose", "The Crown and Lion"],
     corectAnswer: "The Stag and Lion",
     img: "assets/media/images/inn.jpg"
